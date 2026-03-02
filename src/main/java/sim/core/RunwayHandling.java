663890da919d65270febdbfae31250aa5d5129fd
Ben Heaton <u5531686@warwick.ac.uk>
2026-03-02 11:31:09 +0000

    feat: aircraft in holding pattern are now at the correct altitude

--- src/main/java/sim/core/RunwayHandling.java
+++ src/main/java/sim/core/RunwayHandling.java
@@ -13,16 +13,22 @@ public class RunwayHandling{
                             List<Runway> runways,
                             List<Aircraft> postProcessing,
                             Clock clock){
+        // Moves all aircraft which have arrived into the holding pattern                   
         boolean flag = true;
         while (flag)  {
             flag = moveToHoldingPattern(arrivals,holdingPattern,clock);
-        }               
+        } 
+        // Moves all aircraft ready to depart into the take off queue                       
         flag = true;
         while (flag){
             flag = moveToTakeOff(departures,takeOffQueue,clock);
         }
+        // Assigns all aircraft in the take-off queue and holding pattern to a runway
+        // Repeats this process until no aircraft from either are assigned a runway
+        // This will happen either when both stores are empty, or there are no available runways
         boolean takeOffFlag = true;
         boolean landingFlag = true;
+         // Loops as long as one of the stores is being emptied
         while (takeOffFlag || landingFlag){
             if (landingFlag){
                 landingFlag = landPlane(holdingPattern,runways,postProcessing,clock);
@@ -31,8 +37,28 @@ public class RunwayHandling{
                 takeOffFlag = takeOff(takeOffQueue,runways,postProcessing,clock);
             }
         }
+        adjustAltitude(holdingPattern);
                             }
 
+        public void adjustAltitude(HoldingPattern<Aircraft> holdingPattern){
+            int i = 1;
+            LinkedListElement<Aircraft> ptr = holdingPattern.getEmergency().getHead();
+            while (ptr != null){
+                ptr.getValue().setAltitude(i * 1000);
+                i++;
+            }
+            ptr = holdingPattern.getNonEmergency().getHead();
+            while (ptr != null){
+                ptr.getValue().setAltitude(i * 1000);
+                i++;
+            }
+        }
+
+
+         // If the aircraft on top of the arrivals list has arrived at the airport, 
+        // pop it from the list and add it to the holding pattern
+        // Returns true on succesfully moving the aircraft
+        // Returns false if there is no such aircraft to move
         public boolean moveToHoldingPattern(List<Aircraft> arrivals,HoldingPattern<Aircraft> holdingPattern,Clock clock){
             if (arrivals.get(0).getValue().getTime().compareTo(clock.simulationTime) <= 0){
                 LinkedListElement<Aircraft> arrival = new LinkedListElement<>();
@@ -43,6 +69,10 @@ public class RunwayHandling{
             return false;
         }
 
+         // If the aircraft on top of the departure list is ready to depart the airport, 
+        // pop it from the list and add it to the take off queue
+        // Returns true on succesfully moving the aircraft
+        // Returns false if there is no such aircraft to move
         public boolean moveToTakeOff(List<Aircraft> departures,List<Aircraft> takeOffQueue,Clock clock){
             if (departures.get(0).getValue().getTime().compareTo(clock.simulationTime) <= 0){
                 LinkedListElement<Aircraft> departure = new LinkedListElement<>();
@@ -53,6 +83,11 @@ public class RunwayHandling{
             return false;
         }
 
+         // Searches through runways to find an eligible runway
+        // Searches first for single-mode runways, then for dual mode runways
+        // This saves the dual mode runways if they need to be used for taking off
+        // Once a runway is found, land the plane and return true
+        // Returning false indicates either there is no plane to land or there are no runways to land on
         public boolean landPlane(HoldingPattern<Aircraft> holdingPattern,List<Runway> runways,List<Aircraft> postProcessing,Clock clock){
             if (holdingPattern.getSize() == 0){
                 return false;
@@ -84,6 +119,11 @@ public class RunwayHandling{
 
         }
 
+        // Searches through runways to find an eligible runway
+        // Searches first for single-mode runways, then for dual mode runways
+        // This saves the dual mode runways if they need to be used for departing
+        // Once a runway is found, depart the plane and return true
+        // Returning false indicates either there is no plane to depart or there are no runways to take off on
         public boolean takeOff(List<Aircraft> takeOffQueue,List<Runway> runways,List<Aircraft> postProcessing,Clock clock){
             if (takeOffQueue.getSize() == 0){
                 return false;
@@ -115,4 +155,6 @@ public class RunwayHandling{
 
         }
 }   
+
+
                         
